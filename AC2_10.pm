42ebc50c8a034f526f747bfe985ec08cb942fe40
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-05-27 23:17:29 +0200

    Rewrite point in begin block to insert_point

--- AC2_10.pm
+++ AC2_10.pm
@@ -2493,8 +2493,7 @@ sub _read {
     my ($self) = @_;
 
     $self->{entity_common} = CAD::Format::DWG::AC2_10::EntityCommon->new($self->{_io}, $self, $self->{_root});
-    $self->{x} = $self->{_io}->read_f8le();
-    $self->{y} = $self->{_io}->read_f8le();
+    $self->{insert_point} = CAD::Format::DWG::AC2_10::Point2d->new($self->{_io}, $self, $self->{_root});
 }
 
 sub entity_common {
@@ -2502,14 +2501,9 @@ sub entity_common {
     return $self->{entity_common};
 }
 
-sub x {
+sub insert_point {
     my ($self) = @_;
-    return $self->{x};
-}
-
-sub y {
-    my ($self) = @_;
-    return $self->{y};
+    return $self->{insert_point};
 }
 
 ########################################################################
